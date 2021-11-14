25d8a167b3a716456b30a21a791132947c9fb429
Giga Hidjrika <52191436+Infinicus@users.noreply.github.com>
2021-11-14 17:39:02 +0700

    Added Uno + Choose a Candidate

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,7 @@ import Rectangle_1 from './rectangle1.png';
 import Rectangle_2 from './rectangle2.png';
 import Calon_1 from './calon_1.png';
 import Calon_2 from './calon_2.png';
+import ChooseCandidate from './choosecandidate.png';
 
 export{
   Dice,
@@ -10,4 +11,5 @@ export{
   Rectangle_2,
   Calon_1,
   Calon_2,
+  ChooseCandidate,
 }
